0c5c1c12d74cc7580dd5f48f660ef03e5f07958e
Mat Jordan <mat@northwestern.edu>
2022-05-03 16:10:01 -0400

    Update resolution.

--- components/content/ProblemStatement.jsx
+++ components/content/ProblemStatement.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import Image from "next/image";
-import testImg from "../../public/images/clover-screenshot-react.png";
-import CodeSample from "./CodeSample";
-import { Col, TwoColLayout } from "./Cols";
 
 const ProblemStatement = () => (
   <>
-    <h2>IIIF Driven Content</h2>
+    <h2>That's how... but why?</h2>
     <ul>
-      <li>(Nearly) everything can be in a Manifest</li>
       <li>
-        Our infrastructure <strong>should</strong> be stronger and more
-        interoperable.
+        Building and using IIIF manifest for own front end makes our IIIF
+        resources even more robust in the wild and for researchers.
       </li>
       <li>
-        IIIF Presentation 3.0 API provides a ready to use and vetted data
-        schema.
+        The IIIF Presentation 3.0 API speciifcations provides a ready to use and
+        vetted data schema.
       </li>
-      <li>Frontend IIIF components benefit others as well as us.</li>
+      <li>Creating Frontend IIIF components benefit others as well as us.</li>
     </ul>
   </>
 );
