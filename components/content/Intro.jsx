7735ca673ff5d9f5ada5b073fb06acb19808a7b9
Mat Jordan <mat@northwestern.edu>
2022-05-03 14:08:49 -0400

    Edit intro and ecosystem.

--- components/content/Intro.jsx
+++ components/content/Intro.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Image from "next/image";
-import work1 from "../../public/images/work.png";
-import work2 from "../../public/images/work-annotated.png";
+import wireframe from "../../public/images/work.png";
+import annotated from "../../public/images/work-annotated.png";
+import ecosystem from "../../public/images/ecosystem.png";
 import { Col, TwoColLayout } from "./Cols";
 
-const Intro = ({ image = "work" }) => {
-  return (
-    <>{image === "work" ? <Image src={work1} /> : <Image src={work2} />}</>
-  );
+const Intro = ({ image }) => {
+  switch (image) {
+    case "wireframe":
+      return <Image src={wireframe} />;
+    case "annotated":
+      return <Image src={annotated} />;
+    case "ecosystem":
+      return <Image src={ecosystem} />;
+    default:
+      return <Image src={wireframe} />;
+  }
 };
 
 export default Intro;
