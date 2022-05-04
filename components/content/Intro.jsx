f349fe788b8e0c84e9eb10a9a48e778146183218
Mat Jordan <mat@northwestern.edu>
2022-05-03 23:04:28 -0400

    Rearrange, add explode manifest graphic.

--- components/content/Intro.jsx
+++ components/content/Intro.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import explode from "../../public/images/explode.jpg";
 import wireframe from "../../public/images/work.png";
 import annotated from "../../public/images/work-annotated.png";
 import ecosystem from "../../public/images/ecosystem.png";
@@ -7,6 +8,24 @@ import { Col, TwoColLayout } from "./Cols";
 
 const Intro = ({ image }) => {
   switch (image) {
+    case "explode":
+      return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div>
+            <Image src={explode} />
+          </div>
+          <ol>
+            <li>One viewer cannot handle it all</li>
+            <li>
+              <strong>Explode</strong> the manifest, think properties
+            </li>
+            <li>
+              The <em>viewer</em> is the <code>&lt;/main&gt;</code>
+            </li>
+            <li>IIIF properties translate as DOM elements</li>
+          </ol>
+        </div>
+      );
     case "wireframe":
       return <Image src={wireframe} />;
     case "annotated":
