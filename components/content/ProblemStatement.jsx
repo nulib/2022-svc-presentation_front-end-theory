b85c2d535e41416084687dccdbc2b98328920a56
Mat Jordan <mat@northwestern.edu>
2022-05-04 09:26:24 -0400

    Fix typos, add image to theory.

--- components/content/ProblemStatement.jsx
+++ components/content/ProblemStatement.jsx
@@ -5,12 +5,12 @@ const ProblemStatement = () => (
     <h2>That's how... but why?</h2>
     <ul>
       <li>
-        Building and using IIIF manifest for own front end makes our IIIF
-        resources even more robust in the wild and for researchers.
+        Using IIIF manifests for own front end makes our IIIF resources even
+        more robust in the wild and for researchers.
       </li>
       <li>
-        The IIIF Presentation 3.0 API speciifcations provides a ready to use and
-        vetted data schema.
+        The IIIF Presentation 3.0 API speciifcations provides a ready to use
+        data schema.
       </li>
       <li>Creating Frontend IIIF components benefit others as well as us.</li>
     </ul>
